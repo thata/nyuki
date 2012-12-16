7d26a736335bab18f0006362267a8c721cde935d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2012-12-16 23:23:47 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nyuki::Application.config.secret_token = '3cfeee901c251eb2a16ff395b540e403c8cb20c4fd23c8a99efbf0c1c84f28b82cf2fd0f5e813e6eac89c44785d4a50b1f447bda54196bbef608f9d79b060392'
